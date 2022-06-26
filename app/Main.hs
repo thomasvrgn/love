3ddf9f018bad91e7df1ff3d2d7769a8f09ed8574
thomasvergne <thomas.vergne.contact@gmail.com>
2022-06-26 19:26:35 +0200

    ⚡️ Improved main program entry.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,15 @@
 module Main where
   import Text.Parsec (runParser)
   import Parser (parser)
-
+  import IR (fromStmt)
+  
   main :: IO ()
   main = do
     input <- readFile "tests/example"
     let p = runParser parser () "" input
-    print p
+    case p of
+      Left err -> print err
+      Right x -> do
+        print x
+        let js = fromStmt x
+        writeFile "tests/example.js" js
